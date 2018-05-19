29b13439172fb232dee42c4ca25e7b59833a8ca4
Your Name <you@example.com>
2018-05-19 13:34:00 -0400

    update movie.html

--- js/main.js
+++ js/main.js
@@ -19,14 +19,73 @@ axios.get('http://www.omdbapi.com?s='+searchText)
         <div class="well text-center">
           <img src="${movie.Poster}">
           <h5>${movie.Title}</h5>
-          <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href
+          <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" ="#">Movie Details</a>
         </div>
       </div>
     `;
 
   });
+
+  $('#movies').html(output);
 })
 .catch((err) => {
   console.log(err);
 });
+}
+
+function movieSelected(id){
+sessionStorage.setItem('movieId', id);
+window.location = "movie.html";
+return false;
+
+}
+
+function getMovie(){
+  let movieID = sessionStorage.getItem('movieId');
+
+  axios.get('http://www.omdbapi.com?i='+movieId)
+  .then((response) => {
+    console.log(response);
+    let movie = response.data;
+
+let output =`
+  <div class="row">
+    <div class="col-md-4">
+      <img src="${movie.Poster}" class="thumbnail">
+
+    </div>
+    <div class="col-md-8">
+      <h2>${movie.Title}</h2>
+      <ul class="list-group">
+        <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+        <li class="list-group-item"><strong>Released:</strong> ${movie.Genre}</li>
+        <li class="list-group-item"><strong>Rated</strong> ${movie.Genre}</li>
+        <li class="list-group-item"><strong>IMBD Rating:</strong> ${movie.Genre}</li>
+        <li class="list-group-item"><strong>Director:</strong> ${movie.Genre}</li>
+        <li class="list-group-item"><strong>Writers:</strong> ${movie.Genre}</li>
+        <li class="list-group-item"><strong>Actors:</strong> ${movie.Genre}</li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="well">
+      <h3>Plot</h3>
+      ${movie.Pliot}
+    <hr>
+      <a href="http://imdb.com/title/${movie.imdbID}" target=" blank" class="btn btn-primary">View IMDB</a>
+      <a href="index.html" class="btn btn-default">Go Back to Search</a>
+
+
+
+`;
+
+$('#movie').html(output);
+
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  }
+
 }
